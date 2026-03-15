7ad76d69f659c9503fca36afdcc8e79f2e4525c1
MISCHA-PC\muluc <mishaprudnikov11@gmail.com>
2026-03-15 20:04:44 +0100

    acceleration and deceleration

--- scripts/Movement.cs
+++ scripts/Movement.cs
@@ -1,3 +1,4 @@
+using System;
 using Godot;
 
 namespace NewGameProject.scripts;
@@ -5,8 +6,19 @@ namespace NewGameProject.scripts;
 public partial class Movement : Godot.Sprite2D
 {
 	[Export]
-	private float _speed = 10f;
-	// Called when the node enters the scene tree for the first time.
+	private float _maxSpeed = 10f;
+	[Export] 
+	private Curve _accelerationCurve; // The graph slot
+	[Export] 
+	public float AccelTime = 1.0f; 
+	[Export] 
+	private Curve _deccelerationCurve; // The graph slot
+	[Export]
+	public float DeccelTime  = 1.0f;
+	
+	private float _currentAccelTime = 0f;
+	private Vector2 _lastDirVector = Vector2.Zero;
+	
 	public override void _Ready()
 	{
 	}
@@ -15,10 +27,19 @@ public partial class Movement : Godot.Sprite2D
 	public override void _Process(double delta)
 	{
 		var direction = Input.GetVector("move_left", "move_right", "move_up", "move_down").Normalized();
-		if (direction.Length()> 0.2)
+		var deltaF = (float)delta;
+		if (direction.Length() > 0.2)
 		{
-			this.Position += direction * _speed * (float)delta;
+			_lastDirVector = direction;
+			_currentAccelTime = Mathf.Min(_currentAccelTime + deltaF / AccelTime, 1.0f);
+			var deltaPos = 100 *direction * _maxSpeed * deltaF * _accelerationCurve?.Sample(_currentAccelTime) ?? Vector2.Zero;
+			Position += deltaPos ;
 		}
-		
+		else
+		{
+			_currentAccelTime = Math.Max(_currentAccelTime - deltaF / DeccelTime, 0f);
+			var deltaPos =  100 * _maxSpeed * deltaF* _lastDirVector * _deccelerationCurve?.Sample(1-_currentAccelTime) ?? Vector2.Zero;
+			Position += deltaPos ;
+		}
 	}
 }
