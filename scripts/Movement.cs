d65d7f5aeb53142c4b340d71beffa42f0f81eb5a
Mihail Prudnikov <mishaprudnikov11@gmail.com>
2026-03-20 14:00:11 +0100

    rewritten the entire movement logic

--- scripts/Movement.cs
+++ scripts/Movement.cs
@@ -5,41 +5,53 @@ namespace NewGameProject.scripts;
 
 public partial class Movement : Godot.Sprite2D
 {
+	[ExportGroup("Movement Properties")]
 	[Export]
 	private float _maxSpeed = 10f;
-	[Export] 
-	private Curve _accelerationCurve; // The graph slot
-	[Export] 
-	public float AccelTime = 1.0f; 
-	[Export] 
-	private Curve _deccelerationCurve; // The graph slot
 	[Export]
-	public float DeccelTime  = 1.0f;
+	private float _deccelTime  = 1.0f;
+	[Export]
+	private float _accelTime = 1.0f;
+
+	[Export] private float _minGlideSpeed = 0.2f;
 	
-	private float _currentAccelTime = 0f;
-	private Vector2 _lastDirVector = Vector2.Zero;
-	
+	private Vector2 _velocity = Vector2.Zero;
+		
 	public override void _Ready()
 	{
+		
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		
 		var direction = Input.GetVector("move_left", "move_right", "move_up", "move_down").Normalized();
 		var deltaF = (float)delta;
-		if (direction.Length() > 0.2)
+		Vector2 target = direction * _maxSpeed;
+		float steeringForce;
+		float similarity = _velocity.Normalized().Dot(direction);
+		if (direction.Length() < 0.1f)
 		{
-			_lastDirVector = direction;
-			_currentAccelTime = Mathf.Min(_currentAccelTime + deltaF / AccelTime, 1.0f);
-			var deltaPos = 100 *direction * _maxSpeed * deltaF * _accelerationCurve?.Sample(_currentAccelTime) ?? Vector2.Zero;
-			Position += deltaPos ;
+			steeringForce = 1.0f / _deccelTime;
+		}
+		else if(similarity > 0.7f)
+		{
+			steeringForce = 1.0f / (_accelTime * 0.2f);
 		}
 		else
 		{
-			_currentAccelTime = Math.Max(_currentAccelTime - deltaF / DeccelTime, 0f);
-			var deltaPos =  100 * _maxSpeed * deltaF* _lastDirVector * _deccelerationCurve?.Sample(1-_currentAccelTime) ?? Vector2.Zero;
-			Position += deltaPos ;
+			steeringForce = 1.0f /_accelTime;
 		}
+		_velocity = _velocity.Lerp(target, steeringForce * deltaF);
+		GD.Print(_velocity.Length() * 100 *deltaF);
+		if (_velocity.Length() * 100 *deltaF < _minGlideSpeed && direction.Length() < 0.1f)
+		{
+			GD.Print("to slow, stop the player");
+			_velocity = Vector2.Zero;
+		}
+		
+		Position += _velocity * 100* deltaF;
+		
 	}
 }
